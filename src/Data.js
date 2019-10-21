bb574f61e818454aa1e020a62df95769ae2f964c
Voytovich-e <56820912+Voytovich-e@users.noreply.github.com>
2019-10-21 17:21:37 +0200

    refactor: add default img

--- src/Data.js
+++ src/Data.js
@@ -5,7 +5,15 @@ function Data(props) {
   return (
     <Col xs={12} md={6}>
       <div className="card">
-        <img src={props.poster} alt="Avatar" />
+        <img
+          src={props.poster}
+          onError={e => {
+            e.target.onerror = null;
+            e.target.src =
+              "https://i1.wp.com/newnjstatefilmfestival.com/wp-content/uploads/2017/03/camera-2.jpg";
+          }}
+          alt="Avatar"
+        />
         <div className="container">{props.title}</div>
       </div>
     </Col>
--- src/Loader.js
+++ src/Loader.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { Spin, Icon } from "antd";
+import "./styles.css";
 
-const antIcon = <Icon type="loading" style={{ fontSize: 24 }} spin />;
+const antIcon = (
+  <Icon className="load" type="loading" style={{ fontSize: 24 }} spin />
+);
 
 function Loader(props) {
   return <Spin indicator={antIcon} />;
